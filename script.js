24c5a4b3420301a1e6510c8f24a4336331212105
IkkyPT <ricardoo1998carvalho@gmail.com>
2023-03-19 03:55:07 +0000

    +Add basic math functions Added basic math functions as an object and defined add, subtract, multiply, and divide functions. These functions can be used for simple arithmetic operations.

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 // Math basic functions
+const mathFunction = {
+    add,
+    subtract,
+    multiply,
+    divide,
+}
 
-// Add
+// Add function
+function add(a, b){
+    return a + b;
+}
+
+// Subtract function 
+function subtract(a, b){
+    return a - b;
+}
+
+// Multiply function
+function multiply(a, b){
+    return a * b;
+}
+
+// Divide function
+function divide(a, b){
+    return a / b;
+}
+
+console.log(mathFunction.add(2, 3));
+console.log(mathFunction.subtract(2, 3));
+console.log(mathFunction.multiply(2, 3));
+console.log(mathFunction.divide(8, 2));
