8fd42476f19a1c15509470874bbfad29f7e7fb85
IkkyPT <ricardoo1998carvalho@gmail.com>
2023-03-20 11:36:23 +0000

    +Add 3 functions getResult(), getValues(), clearBoard() 1- Add getResult() function which retrieves the values of 'a', 'b', 'operator' by calling the getValues() function. It then calculates the result using the operate() function and the current operator, a, and currValue. The display is then updated with the result, prevResult is cleared, and the values of a, b, and currValue are updated accordingly. 2- Add getValues() function which returns the values of 'a' 'b' and 'operator'. 3- Add clearBoard() function which clears the display by setting the currResult and prevResult text content to an empty string and resetting currValue to 0.

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
 const numberData = document.querySelectorAll('[data-numbers]');
 const btnNumber = document.querySelectorAll('.btnNums');
 const currResult = document.querySelector('.curr-Result');
+const numberOperator = document.querySelectorAll('[data-operator]');
+const btnOperator = document.querySelectorAll('.btnOper');
+const prevResult = document.querySelector('.prev-Result');
+const equalsBtn = document.querySelector('.btnEqual');
+let a = 0;
+let b = 0;
+let currValue = 0;
+let nextValue = 0;
+let operatorType = '';
 
 // Object with group of operate functions
 const mathFunction = {
@@ -30,7 +39,7 @@ function multiply(a, b){
 // Divide function
 function divide(a, b){
     return a / b;
-}
+};
 
 // Operate function
 function operate(operator, a, b){
@@ -39,16 +48,47 @@ function operate(operator, a, b){
             return mathFunction.add(a, b)
         case '-':
             return mathFunction.subtract(a, b);
-        case '*':
+        case 'x':
             return mathFunction.multiply(a, b);
-        case '/':
+        case '÷':
             return mathFunction.divide(a, b);
         default:
             return null;
+    };
+};
+
+// Get result function
+function getResult(){
+    let { a, b, operator } = getValues();
+    b = currValue;
+    let result = operate(operator, a, b);
+    currResult.textContent = result;
+    prevResult.textContent = '';
+    a = result;
+    b = 0;
+    currValue = result;
+}
+
+// Return values function
+function getValues() {
+    return {
+      a,
+      b,
+      operator
     }
 }
 
+// Clear function
+
+function clearfunc(){
+    currResult.textContent = "0";
+    prevResult.textContent = "";
+    currValue = 0;
+};
+
+
 // UI - Display
+  // Add numbers to display once number buttons are clicked
 numberData.forEach(btnNumber => {
     btnNumber.addEventListener('click', () => {
         const number = btnNumber.getAttribute('data-numbers');
@@ -57,7 +97,26 @@ numberData.forEach(btnNumber => {
         } else {
             currResult.textContent += number;
         }
-        let currNum = currResult.textContent;
-        console.log(currNum);
+        currValue = parseFloat(currResult.textContent);
     });
+});
+
+numberOperator.forEach(btnOperator => {
+    btnOperator.addEventListener('click', () => {
+        const operatorType = btnOperator.getAttribute('data-operator');
+        if (currResult.textContent !== ""){
+            currResult.textContent += operatorType;
+            prevResult.textContent += currResult.textContent;
+            currResult.textContent = "";
+            operator = operatorType;
+        }
+        else {
+            return null;
+        }
+        a = currValue;
+    });
+});
+
+equalsBtn.addEventListener('click', () => {
+    getResult();
 });
