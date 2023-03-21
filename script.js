6f4b379e524a85d2b83a4841c2499757a844b0e2
IkkyPT <ricardoo1998carvalho@gmail.com>
2023-03-21 00:41:38 +0000

    + Fix bug in order of operation - Remove getValues() 1- Previously, the calculator was not correctly following the order of operations, which caused incorrect results for some expressions. Specifically, the 'operator' variable was using the current selected parameters instead of the previous ones to calculate from two to two. This commit fixes this bug by rearranging the code to first update the 'a' and 'b' variables before updating the 'operator' variable. The code has been tested and verified to correctly follow the order of operations for various expressions. 2- Removed getValues() function. 3- Reset some additional needed values in clearBoard() function.

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ const numberOperator = document.querySelectorAll('[data-operator]');
 const btnOperator = document.querySelectorAll('.btnOper');
 const prevResult = document.querySelector('.prev-Result');
 const equalsBtn = document.querySelector('.btnEqual');
-let a = 0;
-let b = 0;
+let a ;
+let b ;
 let currValue = 0;
-let nextValue = 0;
 let operatorType = '';
+let multiOperaOn = false;
+let result;
 
 // Object with group of operate functions
 const mathFunction = {
@@ -44,14 +45,14 @@ function divide(a, b){
 // Operate function
 function operate(operator, a, b){
     switch (operator) {
-        case '+':
-            return mathFunction.add(a, b)
-        case '-':
-            return mathFunction.subtract(a, b);
         case 'x':
             return mathFunction.multiply(a, b);
         case '÷':
             return mathFunction.divide(a, b);
+        case '+':
+            return mathFunction.add(a, b)
+        case '-':
+            return mathFunction.subtract(a, b);
         default:
             return null;
     };
@@ -59,9 +60,8 @@ function operate(operator, a, b){
 
 // Get result function
 function getResult(){
-    let { a, b, operator } = getValues();
     b = currValue;
-    let result = operate(operator, a, b);
+    result = operate(operator, a, b);
     currResult.textContent = result;
     prevResult.textContent = '';
     a = result;
@@ -69,21 +69,14 @@ function getResult(){
     currValue = result;
 }
 
-// Return values function
-function getValues() {
-    return {
-      a,
-      b,
-      operator
-    }
-}
-
 // Clear function
-
-function clearfunc(){
+function clearBoard(){
     currResult.textContent = "0";
     prevResult.textContent = "";
     currValue = 0;
+    a = 0;
+    result = 0;
+    multiOperaOn = false;
 };
 
 
@@ -104,6 +97,17 @@ numberData.forEach(btnNumber => {
 numberOperator.forEach(btnOperator => {
     btnOperator.addEventListener('click', () => {
         const operatorType = btnOperator.getAttribute('data-operator');
+        if (multiOperaOn == true){
+            b = currValue;
+            result = operate(operator, a, b);
+            a = result;
+            b = 0;
+            currValue = result;
+        }
+        else {
+            a = currValue;
+        }
+        multiOperaOn = true;
         if (currResult.textContent !== ""){
             currResult.textContent += operatorType;
             prevResult.textContent += currResult.textContent;
@@ -113,10 +117,10 @@ numberOperator.forEach(btnOperator => {
         else {
             return null;
         }
-        a = currValue;
     });
 });
 
 equalsBtn.addEventListener('click', () => {
     getResult();
+    multiOperaOn = false;
 });
